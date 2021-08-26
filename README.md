44e656f007ba1db51c4c09fe4bdee243e84eb4b6
yiminprogram <minwork1016@outlook.com>
2021-08-26 16:10:09 +0800

    initial project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vue3-composition-api-todolist
- 
+
+實作 vue3 composition api 並製作基本應用 Todo List
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="$style.container">
+    <h1>Todo List</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" module>
+.container {
+  text-align: center;
+}
+</style>
